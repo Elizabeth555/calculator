211b916251f9e24c1482b65c9925c6672f08fedd
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2017-01-26 12:55:06 +0000

    worksgit add .

--- math.js
+++ math.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
 
-var test = document.querySelector(".clear")
+var keys = document.querySelectorAll(" #calculator span")
+var operators = ['+','-','x','÷']
 
-test.onclick = function(e){
-  console.log("hi")
+for(i = 0; i<keys.length; i++ ) {
+  keys[i].onclick = function(e) {
+    var screenView = document.querySelector('.screen');
+    var screenText = screenView.innerHTML;
+    var btnVal = this.innerHTML;
+
+    if(btnVal == 'C') {
+      screenView.innerHTML = ""
+    }
+
+    else if (btnVal == '=') {
+      var equation = screenText;
+      equation = equation.replace(/x/g,'*')
+                        .replace(/÷/g, '/');
+
+    if(equation)
+      screenView.innerHTML = eval(equation);
+    }
+
+    else{
+      screenView.innerHTML += btnVal;
+    }
+
+  }
 }
 
 
